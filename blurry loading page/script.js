c0a1718d1a4902c3fd6e89d3cb1612b75a52d622
Praveen Thamindu <praveenthamindu@gmail.com>
2022-02-10 03:27:28 +0530

    blur loading

---
+++ blurry loading page/script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const loadingText = document.querySelector(".loading-text");
+
+let load = 0;
+
+let inter = setInterval(blurring, 30);
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+function blurring() {
+    load++;
+
+    if (load > 99) {
+        clearInterval(inter);
+    }
+
+    loadingText.innerText = `${load}%`;
+    loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+    container.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
