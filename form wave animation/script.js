ddbeae057c82ee8e9e5394dcd372d33599024380
Praveen Thamindu <praveenthamindu@gmail.com>
2022-02-10 21:22:15 +0530

    fix input feild

--- form wave animation/script.js
+++ form wave animation/script.js
@@ -1,7 +1,21 @@
 const labels = document.querySelectorAll("label");
+const textEle = document.querySelectorAll("input");
+
 
 labels.forEach(label => {
     label.innerHTML = label.innerHTML.split("")
     .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
     .join("");
+})
+
+textEle.forEach((item) => {
+    item.addEventListener("focus", (e) => {
+        e.target.nextElementSibling.classList.add("wave");
+    });
+
+    item.addEventListener("focusout", (e) => {
+        if (!e.target.value) {
+            e.target.nextElementSibling.classList.remove("wave");
+        }
+    })
 })
