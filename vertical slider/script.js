ceb47610d4ad80ec49bf07714f831cf70d535a57
Praveen Thamindu <praveenthamindu@gmail.com>
2022-02-18 21:52:01 +0530

    vertical slider

---
+++ vertical slider/script.js
@@ -0,0 +1,29 @@
+const donwBtn = document.querySelector(".down-button");
+const upBtn = document.querySelector(".up-button");
+const leftSlider = document.querySelector(".left-slider");
+const rightSlider = document.querySelector(".right-slider");
+const imagesLength = document.querySelectorAll(".left-slider div").length - 1;
+
+let currentIndex = 0;
+
+function transformImages() {
+    leftSlider.style.transform = `translateY(-${(imagesLength - currentIndex) * 100}%)`;
+    rightSlider.style.transform = `translateY(-${currentIndex * 100}%)`;
+}
+
+upBtn.addEventListener("click", () => {
+    currentIndex++;
+    if (currentIndex > imagesLength) {
+        currentIndex = 0;
+    }
+
+    transformImages();
+});
+
+donwBtn.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = imagesLength;
+    }
+    transformImages();
+});
