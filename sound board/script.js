173533fb3159714df3d5a8abfd473f0f6dd13afc
Praveen Thamindu <praveenthamindu@gmail.com>
2022-03-04 14:34:14 +0530

    sound borad

--- sound board/script.js
+++ sound board/script.js
@@ -2,10 +2,22 @@ const soundsEl = document.querySelectorAll("audio");
 const sounds = [...soundsEl].map(item => item.id);
 const btsDiv = document.querySelector(".btns");
 
+function stopPlayingSound() {
+    sounds.forEach(audio => {
+        const playingSound = document.getElementById(audio);
+        playingSound.pause();
+        playingSound.currentTime = 0;
+    })
+}
+
 sounds.forEach(sound => {
     const buttonEl = document.createElement("button");
     buttonEl.classList.add("btn");
     buttonEl.innerText = sound;
+    buttonEl.addEventListener("click", () => {
+        stopPlayingSound();
+        document.getElementById(sound).play();
+    })
 
     btsDiv.append(buttonEl);
 });
