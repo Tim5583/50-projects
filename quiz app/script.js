77b55711c53487bbfc2f2214cf2c13f9ee9d2c58
Praveen Thamindu <praveenthamindu@gmail.com>
2022-03-09 00:13:45 +0530

    add questions

---
+++ quiz app/script.js
@@ -0,0 +1,26 @@
+const quizData = [
+    {
+        question: "Which language runs in web browser?",
+        a : "Jave",
+        b : "C",
+        c : "Python",
+        d : "JavaScript",
+        correct: "d"
+    },
+    {
+        question: "What does CSS stand for?",
+        a : "Central Style Sheets",
+        b : "Cascading style sheets",
+        c : "Cascadomg Simple Sheets",
+        d : "Cars SUVs Sailboats",
+        correct: "b"
+    },
+    {
+        question: "What does HTML stand for?",
+        a : "Hypertext Markup Language",
+        b : "Hypertext Markdown Language",
+        c : "Hyperloop Machine Language",
+        d : "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a"
+    }
+]
